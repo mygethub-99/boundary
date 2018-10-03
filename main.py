50a7246ffcbad82d6bfe53cd08871919bde42861
mygethub-99 <tomakeitcount@gmail.com>
2018-10-02 23:42:31 -0500

    Need to clean up input of " sym

--- main.py
+++ main.py
@@ -5,25 +5,24 @@ Multiband cross check
 @author: owen
 """
 import check_member as check
-
-l = [["Mobile",1097,	"E2,E1,F2,F1","a"	,"B,C","I2,I1,J3,J4,J5,J6",13805304,"D2,D1,F2,F1,C6,C7","none","C","C2,C1,F3,F4,F5,F6,J3,J4,J5,J6"],
-     ["East Carroll",22035,"D2,D1,E2,E1,C9,C10,C11","b","B,C","F3,F4,F5,F6,I2,I1,J3,J4,J5,J6",14511289,"B6,B7,B8,B9,B10,B11,E2,E1,C","none","B,C","J3,J4,J5,J6"]]
 output = [] #collects audit returned from check_member.py
-#with open('shortlist2.txt') as f:
-    #next(f)
-x = 6
-y = 2 
-for i in l:
-    line = l
-    count = 4
-    holding = []
-    holding.append(i[1])
-    holding.append(i[5])
-    while count > 0:
-        for val in line:
-            missing = check.checks(val[x],val[y])
-            holding.append(missing)
-            x = x+1
-            y = y+1
-            count = count - 1
-output.append(holding)
+with open('query5mile.txt') as f:
+    next(f)
+     
+    for i in f:
+        i = i.split('\t')
+        count = 4
+        holding = []
+        holding.append(i[1]), holding.append(i[6])
+        f1900 = [i[7],i[2]]
+        f850 = [i[8],i[3]]
+        f700 = [i[9],i[4]]
+        faws = [i[10],i[5]]
+        combine = [f1900,f850,f700,faws]
+        
+        while count > 0:
+            for val in combine:
+                checkup = check.checks(val[0],val[1])
+                holding.append(checkup)
+                count = count - 1
+            output.append(holding)
