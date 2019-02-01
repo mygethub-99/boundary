83a4e080a2285bce18b9e2596800a1c5f812996a
mygethub-99 <tomakeitcount@gmail.com>
2019-02-01 13:20:22 -0600

    Update main.py

--- main.py
+++ main.py
@@ -14,17 +14,19 @@ fn1 = fn+'.txt'
 with open(fn1) as f:
     next(f)
 
+
 #Group SpectrumLandscape and cell site, by band, for comparison     
     for i in f:
         i = i.replace('\n','')
         i = i.split('\t')
         count = 4
         holding = []
-        holding.append(i[1]), holding.append(i[6])
-        f1900 = [i[7],i[2]]
-        f850 = [i[9],i[3]]
-        f700 = [i[8],i[4]]
-        faws = [i[10],i[5]]
+        holding.append(i[1]),holding.append(i[6]),holding.append(i[7]),\
+        holding.append(i[8])        
+        f1900 = [i[9],i[2]]
+        f850 = [i[11],i[3]]
+        f700 = [i[10],i[4]]
+        faws = [i[12],i[5]]
         combine = [f1900,f850,f700,faws]
 
 #Loop through each combine list, one band at a time, and appends holding with results       
@@ -37,7 +39,7 @@ with open(fn1) as f:
             output.append(holding)
 fn = fn+'output.csv'        
 with open(fn, 'w') as fs:
-    fs.write('FIPS,''FA,' '1900_Offender,' '850_Offender,' '700_Offender,' 'AWSOffeder')
+    fs.write('FIPS,''FA,''Lat,' 'Long,' '1900_Offender,' '850_Offender,' '700_Offender,' 'AWSOffeder')
     fs.write('\n')
     for i in output:
         i = str(reduce(lambda x,y:x+","+y,i))
